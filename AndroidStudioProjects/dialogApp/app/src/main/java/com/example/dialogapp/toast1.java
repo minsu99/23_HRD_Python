ecbd9d0f6909dcea8c4858ab51a95a46ca526c2f
Kang <114234771+minsu99@users.noreply.github.com>
2023-09-15 15:46:03 +0900

    Add files via upload

---
+++ AndroidStudioProjects/dialogApp/app/src/main/java/com/example/dialogapp/toast1.java
@@ -0,0 +1,14 @@
+package com.example.dialogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class toast1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toast1);
+    }
+}
