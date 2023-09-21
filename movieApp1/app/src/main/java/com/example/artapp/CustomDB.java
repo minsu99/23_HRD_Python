6573fb8ff1e15d5f6f47422f1247f29ba7655e68
Kang <114234771+minsu99@users.noreply.github.com>
2023-09-21 11:06:38 +0900

    Add files via upload

---
+++ movieApp1/app/src/main/java/com/example/artapp/CustomDB.java
@@ -0,0 +1,91 @@
+package com.example.artapp;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class CustomDB extends SQLiteOpenHelper {
+
+    private Context context;
+    public static final String DATABASE_NAME = "movie123.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_NAME = "phone_info";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "phone_name";
+    public static final String COLUMN_PHONE_NUMBER = "art_name";
+    public static final String COLUMN_RATING = "rating";
+
+    public static final String COLUMN_REVIEW = "review";
+
+
+    public static final String COLUMN_PHONE = "phone_number"; // COLUMN_PHONE을 COLUMN_PHONE_NUMBER로 변경
+
+
+    public CustomDB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " TEXT, " +
+                COLUMN_PHONE_NUMBER + " TEXT, " +
+                "rating REAL DEFAULT 0.0, " +
+                COLUMN_REVIEW + " TEXT);"; // "review" 열 추가
+        db.execSQL(query);
+    }
+
+
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+
+
+    /**
+     * 연락처 전체 가져오기
+     *
+     * @return
+     */
+    public Cursor readAllData() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor cursor = null;
+        if (db != null) {
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+
+    /**
+     * 작품 등록
+     *
+     * @param name        작가 이름
+     * @param artworkName 작품명
+     * @param rating      평가 값
+     */
+    public void addArtwork(String name, String artworkName, float rating, String review) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_NAME, name);
+        cv.put(COLUMN_PHONE_NUMBER, artworkName);
+        cv.put(COLUMN_RATING, rating);
+        cv.put(COLUMN_REVIEW, review); // 리뷰 추가
+        long result = db.insert(TABLE_NAME, null, cv);
+        if (result == -1) {
+            Toast.makeText(context, "작품 등록 실패", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "작품 등록 성공", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
---
+++ movieApp1/app/src/main/java/com/example/artapp/SecondActivity.java
@@ -0,0 +1,89 @@
+    package com.example.artapp;
+
+    import androidx.appcompat.app.AppCompatActivity;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.ImageView;
+    import android.widget.RatingBar;
+    import android.widget.TextView;
+    import android.widget.Toast;
+
+    public class SecondActivity extends AppCompatActivity {
+
+        private EditText addNameEdit;
+        private EditText addReviewEdit;
+        private RatingBar addRatingBar;
+        private TextView mostVotedTextView;
+        private EditText addArtworkEdit;
+        private RatingBar mostVotedRatingBar;
+        private ImageView mostVotedImageView;
+        private Button btnReturn;
+        private CustomDB customDB;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_second);
+            setTitle("투표 결과");
+
+            addNameEdit = findViewById(R.id.add_name_edit);
+            addReviewEdit = findViewById(R.id.add_review_edit);
+            addRatingBar = findViewById(R.id.add_rating_bar);
+            addArtworkEdit = findViewById(R.id.add_artwork_edit); // 작품명을 입력받을 EditText 추가
+
+
+            btnReturn = findViewById(R.id.btnReturn);
+
+            //mostVotedTextView = findViewById(R.id.mostVotedTextView);
+            mostVotedImageView = findViewById(R.id.mostVotedImageView);
+            mostVotedRatingBar = findViewById(R.id.add_rating_bar);
+
+            customDB = new CustomDB(this);
+
+            btnReturn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // 입력값 변수에 담기
+                    String name = addNameEdit.getText().toString(); // 이름 읽어오기
+                    String artworkName = addArtworkEdit.getText().toString();; // 작품명은 빈 문자열로 설정
+                    float rating = addRatingBar.getRating(); // RatingBar에서 평가값 가져오기
+                    String review = addReviewEdit.getText().toString(); // 리뷰 읽어오기
+
+                    // DB에 이름, 작품명, 별점, 리뷰 추가
+                    customDB.addArtwork(name, artworkName, rating, review); // 리뷰 추가
+
+                    // 저장 성공 메시지 표시
+                    Toast.makeText(SecondActivity.this, "작품 등록 성공", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), Custom.class);
+                    startActivity(intent);
+                }
+            });
+
+
+            Intent intent = getIntent();
+            int[] voteResult = intent.getIntArrayExtra("VoteCount");
+            String[] imageName = intent.getStringArrayExtra("ImageName");
+
+            int maxEntry = 0;
+            for (int i = 1; i < voteResult.length; i++) {
+                if (voteResult[maxEntry] < voteResult[i])
+                    maxEntry = i;
+            }
+
+            Integer imageFileId[] = {
+                    R.drawable.j, R.drawable.kn, R.drawable.ma,
+                    R.drawable.fig, R.drawable.g, R.drawable.mo,
+                    R.drawable.no, R.drawable.one, R.drawable.r
+            };
+
+            String mostVotedArtworkName = imageName[maxEntry];
+            addArtworkEdit.setText(mostVotedArtworkName);
+
+            mostVotedImageView.setImageResource(imageFileId[maxEntry]);
+            //mostVotedTextView.setText(imageName[maxEntry]);
+            mostVotedRatingBar.setRating(voteResult[maxEntry]);
+        }
+    }
